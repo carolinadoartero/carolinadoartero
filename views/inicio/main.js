3bc36553573b7a36467da0f94153c42b7d7547db
cajitafelizz <56460586+cajitafelizz@users.noreply.github.com>
2022-04-11 12:07:24 -0300

    primer commit!

---
+++ views/inicio/main.js
@@ -0,0 +1,14 @@
+// const botonesRedireccion = document.querySelectorAll(".boton-redireccion");
+// const imagenesInicio = document.querySelectorAll(".img-inicio");
+
+// for (let i = 0; i < botonesRedireccion.length; i++) {
+//   botonesRedireccion[i].addEventListener("mouseover", () => {
+//     imagenesInicio[i].classList.add("img-hover-enter");
+//     imagenesInicio[i].classList.remove("img-hover-leave");
+//   });
+
+//   botonesRedireccion[i].addEventListener("mouseleave", () => {
+//     imagenesInicio[i].classList.add("img-hover-leave");
+//     imagenesInicio[i].classList.remove("img-hover-enter");
+//   });
+// }
